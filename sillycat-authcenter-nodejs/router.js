e5d1494a5ca178416d0556947445d9f02c374c12
Hua Luo <luohuazju@gmail.com>
2022-04-09 12:50:08 -0500

    add more in posts

--- sillycat-authcenter-nodejs/router.js
+++ sillycat-authcenter-nodejs/router.js
@@ -12,6 +12,18 @@ const accountController = new AccountController();
 var router = express.Router();
 /**
  * @swagger
+ * definitions:
+ *   Account:
+ *     properties:
+ *       name:
+ *         type: string
+ *       email:
+ *         type: string
+ *       age:
+ *         type: integer
+ */
+/**
+ * @swagger
  * /api/v1/ping:
  *   get:
  *     tags:
@@ -23,9 +35,44 @@ var router = express.Router();
  *         description: return dong
  */
 router.get('/api/v1/ping', pingController.ping);
+/**
+ * @swagger
+ * /api/v1/accounts:
+ *   post:
+ *     tags:
+ *       - account
+ *     description: Create a new Account
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: account
+ *         description: Account Object
+ *         in: body
+ *         required: true
+ *         schema:
+ *           $ref: '#/definitions/Account'
+ *     responses:
+ *       200:
+ *         description: Successfully created
+ */
 router.post('/api/v1/accounts', accountController.add);
 router.put('/api/v1/accounts/:id', accountController.update);
 router.delete('/api/v1/accounts/:id', accountController.delete);
+/**
+ * @swagger
+ * /api/v1/accounts:
+ *   get:
+ *     tags:
+ *       - account
+ *     descriptions: Return all accounts
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: an array of accounts 
+ *         schema:
+ *           $ref: '#/definitions/Account'
+ */
 router.get('/api/v1/accounts', accountController.query);
 router.get('/api/v1/accounts/:id', accountController.get);
 
