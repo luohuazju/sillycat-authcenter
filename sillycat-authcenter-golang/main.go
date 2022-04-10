e016cb900f1fb4f94f96ab1646eff4fb0661c22f
Hua Luo <luohuazju@gmail.com>
2022-04-10 10:36:34 -0500

    add to the cors support

--- sillycat-authcenter-golang/main.go
+++ sillycat-authcenter-golang/main.go
@@ -32,7 +32,7 @@ func SetupRouter() *gin.Engine {
 		gin.SetMode(gin.ReleaseMode)
 	}
 	router := gin.Default()
-
+	router.Use(CORSMiddleware())
 	router.GET("/docs/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
 	router.GET("/ping", health.Ping)
@@ -46,6 +46,23 @@ func SetupRouter() *gin.Engine {
 	}
 
 	return router
+}
+
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+		c.Header("Access-Control-Allow-Origin", "*")
+		c.Header("Access-Control-Allow-Credentials", "true")
+		c.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Header("Access-Control-Allow-Methods", "POST,HEAD,PATCH, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
 }
 
 func main() {
